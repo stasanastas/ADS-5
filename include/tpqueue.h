5a12966f0913901b64768b90d697dda3adbeb429
Анастасия Карова <76094090+stasanastas@users.noreply.github.com>
2021-06-03 23:27:08 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,7 +5,72 @@
 
 template<typename T>
 class TPQueue {
-  // Сюда помещается описание структуры "Очередь с приоритетами"
+  struct ITEM {
+    T data;
+    ITEM *next;
+    int prior;
+  };
+
+ public:
+  TPQueue():head(nullptr), tail(nullptr) {}
+  ~TPQueue();
+  void push(const T&data);
+  T pop();
+  void print() const;
+
+ private:
+  ITEM *head;
+  ITEM *tail;
+  ITEM* create(T data) {
+    ITEM *item = new ITEM;
+    item->data = data;
+    item->next = nullptr;
+    return item;
+  }
+
+  ~TPQueue() {
+    while(head)
+      pop();
+  }
+  void push(const T&data) {
+    if(tail && head) {
+      ITEM *temp = head;
+      if (temp->data.prior < data.prior) {
+        temp = create(data);
+        temp->next = next;
+        head = temp;
+      } else {
+        while (temp->next) {
+          if (temp->next->data.prior < data.prior) {
+            ITEM *item = careate(data);
+            item->next = temp->next;
+            temp->next = item;
+            break;
+          } else {
+            temp = temp->next;
+          }
+        }
+      }
+      if (!temp->next) {
+        tail->next = create(data);
+        tail = tail−>next;
+      }
+    } else {
+      head = create(data);
+      tail = head;
+    }
+  }
+  T pop() {
+    if (head) {
+      ITEM *temp = head−>next;
+      T data = head−>data;
+      delete head;
+      head = temp;
+      return data;
+    } else {
+      return (T)0;
+    }
+  }
 };
 
 struct SYM {
